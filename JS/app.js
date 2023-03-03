aef987496e602cdec37b1b98a4009012337a548a
rajib-sadhu <rahulrajib.sadhu@gmail.com>
2023-03-03 18:36:46 +0530

    add modal

--- JS/app.js
+++ JS/app.js
@@ -38,10 +38,13 @@ const displayData = (data, dataLimit) =>{
     }
     
 
+    // Display data on HTML file
     for(const ai of data){
 
         const aiDiv = document.createElement('div');
         aiDiv.classList.add('col');
+
+        const features = ai.features;
         
         aiDiv.innerHTML=`
         
@@ -51,12 +54,12 @@ const displayData = (data, dataLimit) =>{
             <h3 class="card-title text-center pb-2">${ai.name}</h3>
             <h5>Features</h5>
             <ul class="list-group">
-                 ${ ai.features.map(val => `<li class="features-list" >${val}</li>` ) }
+            ${ features.map(val => `<li class="features-list" >${val.slice(0,-1)}</li>`) }
             </ul>
           </div>
           <div style="background-color: #e6324b;" class="card-footer py-3">
               <small class="text-light"> <i class="fa-regular fa-calendar-days"></i> Published: ${ai.published_in}</small>
-              <button class="btn arrow-btn" ><i class="fa-solid fa-location-arrow"></i></button>
+              <button onclick="modalDetails('${ai.id}')" class="arrow-btn" data-bs-toggle="modal" data-bs-target="#showModalDetails" ><i class="fa-solid fa-location-arrow"></i></button>
             </div>
         </div>
         `;
@@ -65,7 +68,7 @@ const displayData = (data, dataLimit) =>{
 
     isLoadingToggle(false)
 }
-
+// 
 
 // show all data btn
 document.getElementById('btn-show-all').addEventListener('click', ()=>{
@@ -85,6 +88,36 @@ function isLoadingToggle (isLoad){
         load.classList.add('d-none');
 
     }
+}
+
+// Show Modal Details
+async function modalDetails(id){
+
+    const url = `https://openapi.programming-hero.com/api/ai/tool/${id}`;
+    const res = await fetch(url);
+    const data = await res.json();
+    console.log(data);
+    const ai = data.data;
+
+    document.getElementById('showModalDetailsLabel').innerText = ai.tool_name;
+
+    const aiDetails = document.getElementById('ai-details');
+    // phoneDetails.innerHTML=`
+    //         <div class="d-flex gap-4">
+    //             <div class="w-50">
+    //             <img src="${phone.image}" class="img-fluid">
+    //             </div>
+    //             <div>
+    //                 <p> Release Date : ${ phone.releaseDate? phone.releaseDate : 'Release date not found' } </p>
+    //                 <p> Brand : ${ phone.brand? phone.brand : 'Brand name not found' } </p>
+    //                 <p> Memory : ${ phone.mainFeatures.memory? phone.mainFeatures.memory : 'Not found' } </p>
+    //                 <p> Display size : ${ phone.mainFeatures.displaySize ? phone.mainFeatures.displaySize : 'Not found' } </p>
+    //                 <p> Storage : ${ phone.mainFeatures.storage ? phone.mainFeatures.storage : 'Not found' } </p>
+    //             </div>
+    //         </div>
+            
+    // `;
+
 }
 
 
